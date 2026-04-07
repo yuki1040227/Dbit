ad68a597162a29a94d72e4e8035d17d85ac6a409
toobest54 <45444184+toobest54@users.noreply.github.com>
2026-04-07 13:55:53 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,3 @@
-Dbit.set_offset(
--3,
-10,
-5,
-7
-)
-basic.forever(() => {
-    Dbit.do_action(Dbit.action_name.walk, 1, 50)
-    Dbit.do_action(Dbit.action_name.walk_backward, 1, 50)
-    Dbit.do_action(Dbit.action_name.turn_left, 1, 50)
-    Dbit.do_action(Dbit.action_name.turn_right, 1, 50)
-    Dbit.do_action(Dbit.action_name.moonwalk_left, 1, 50)
-    Dbit.do_action(Dbit.action_name.moonwalk_right, 1, 50)
-    Dbit.do_action(Dbit.action_name.shake_left, 1, 50)
-    Dbit.do_action(Dbit.action_name.shake_right, 1, 50)
-    Dbit.do_action(Dbit.action_name.go_up_and_down, 1, 50)
-    Dbit.do_action(Dbit.action_name.swing, 1, 50)
-    Dbit.do_action(Dbit.action_name.walk_boldly, 1, 50)
-    Dbit.do_action(Dbit.action_name.walk_backward_boldly, 1, 50)
-    Dbit.do_action(Dbit.action_name.walk_shyly, 1, 50)
-    Dbit.do_action(Dbit.action_name.walk_backward_shyly, 1, 50)
-    Dbit.do_action(Dbit.action_name.big_swing, 1, 50)
+basic.forever(function () {
+	
 })
